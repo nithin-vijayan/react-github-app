0252f33925ccf3471db11aa3626f49d72f1b435b
Nithin K <vijayan@apple.com>
2018-01-18 23:22:26 +0530

    Created and implemented components for battle avatars

---
+++ app/components/App.js
@@ -0,0 +1,36 @@
+
+var React = require ('react');
+var Popular = require('./Popular');
+var ReactRouter = require('react-router-dom')
+var Router = ReactRouter.BrowserRouter;
+var Switch = ReactRouter.Switch;
+var Route = ReactRouter.Route;
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
+
+
+
+
+
+class App extends React.Component	{
+	render() {
+		return (
+			<Router>
+				<div className = "container">
+					<Nav />
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route path='/battle' component={Battle} />
+						<Route path='/popular' component={Popular} />
+						<Route render={function () {
+							return <p>Not Found</p>
+						}} />
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
+}
+
+module.exports = App ;
