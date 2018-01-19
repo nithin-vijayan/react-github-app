bf525fe21fa99c1de8b989e49f06de92950f64f3
Nithin K <vijayan@apple.com>
2018-01-19 17:10:40 +0530

    Completed battle section. added api calls and ui for battle results

--- app/components/App.js
+++ app/components/App.js
@@ -8,6 +8,7 @@ var Route = ReactRouter.Route;
 var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
+var Results = require('./Results');
 
 
 
@@ -21,7 +22,8 @@ class App extends React.Component	{
 					<Nav />
 					<Switch>
 						<Route exact path='/' component={Home} />
-						<Route path='/battle' component={Battle} />
+						<Route exact path='/battle/' component={Battle} />
+						<Route path='/battle/results' component={Results} />
 						<Route path='/popular' component={Popular} />
 						<Route render={function () {
 							return <p>Not Found</p>
